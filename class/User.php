b4f3bbd3e82f90f8d93b3d5888f6db06be27ee7b
KittenDestroyer <lifestartsrightnow@gmail.com>
2015-01-27 18:00:49 +0200

    userclass

--- class/User.php
+++ class/User.php
@@ -1,56 +1,42 @@
 <?php
-  include ( "Database.php" );
+  include( "Database.php" );
+  include( "advanceUser.php" );
   class User
   {
-  	private $userRoles = array();
+ 	/*private $user_id;
+ 	private $username;
+ 	private $firstName;
+ 	private $lastName;
+ 	private $email;*/
 
-  	public function __construct($user_id)
-  	{
-  		$conn = new Database();
-  		$conn->query("SELECT * FROM user WHERE user_id = :user_id");
-  		$conn->bind( ":user_id", $user_id );
-  		$conn->execute();
+ 	private $userData = array();
 
-  		if($conn->rowCount() == 1)
-  		{
-  			$userData = $conn->singleFetch();
-  			$this->user_id = $user_id;
-  			$this->username = ucfirst( $userData['username'] );
-  			$this->email = $userData['email'];
-  			self::loadRoles();
-  		}
-  	}
+ 	public function __construct( $data = array() )
+ 	{
 
-  	public static function loadRoles()
-  	{
-  		$conn = new Database();
-  		$conn->query("SELECT user_role.role_id, role.role_name FROM user_role JOIN role
-  		ON user_role.role_id = role.role_id WHERE user_role.user_id = :user_id");
-  		$conn->bind( ":user_id", $this->user_id );
-  		$conn->execute();
+ 	}
 
-      $row = $conn->allFetch();
+ 	public function login( $data )
+ 	{
+ 		$conn = new Database();
+ 		$conn->query( "SELECT * FROM user WHERE username = :username" );
+ 		$conn->bind( ":username", $data['username'] );
+ 		$conn->execute();
 
-      foreach ( $row["role_name"] as $rname && $row["role_id"] as $rid )
-      {
-        $this->userRoles[$rname] = Role::getRole($rid);
-      }
+ 		if( $conn->rowCount() == 1 )
+ 		{
+ 			$row = $conn->singleFetch();
+ 			if( $row['password'] == $data['password'] )
+ 			{
+ 				$user = new advanceUser( $data['user_id'] );
+ 				
+ 			}
+ 			else
+ 			{
+ 				return echo "Incorrect password.";
+ 			}
+ 		}
+ 	}
 
-  		/*while($row = $st->fetch( PDO::FETCH_ASSOC ) )
-  		{
-  			$this->userRoles[$row["role_name"]] = Role::getRole($row["role_id"]);
-  		}*/
-  	}
 
-  	public function hasPermission($permission)
-  	{
-  		foreach( $this->userRoles as $role )
-  		{
-  			if( $role->hasPermission($permission) )
-  			{
-  				return true;
-  			}
-  		}
-  		return false;
-  	}
   }
---
+++ class/advanceUser.php
@@ -0,0 +1,64 @@
+<?php
+  include ( "Database.php" );
+  class advanceUser
+  {
+    /*private $user_id;
+    private $username;
+    private $firstName;
+    private $lastName;
+    private $email;*/
+
+  	private $userRoles = array();
+
+  	public function __construct($user_id)
+  	{
+  		$conn = new Database();
+  		$conn->query("SELECT * FROM user WHERE user_id = :user_id");
+  		$conn->bind( ":user_id", $user_id );
+  		$conn->execute();
+
+  		if($conn->rowCount() == 1)
+  		{
+  			$userData        = $conn->singleFetch();
+  			$this->user_id   = $user_id;
+  			$this->username  = ucfirst( $userData['username'] );
+        $this->firstName = ucfirst( $userData['firstName'] );
+        $this->lastName  = ucfirst( $userData['lastName'] );
+  			$this->email     = $userData['email'];
+  			self::loadRoles();
+  		}
+  	}
+
+  	public static function loadRoles()
+  	{
+  		$conn = new Database();
+  		$conn->query("SELECT user_role.role_id, role.role_name FROM user_role JOIN role
+  		ON user_role.role_id = role.role_id WHERE user_role.user_id = :user_id");
+  		$conn->bind( ":user_id", $this->user_id );
+  		$conn->execute();
+
+      $row = $conn->allFetch();
+
+      foreach ( $row["role_name"] as $rname && $row["role_id"] as $rid )
+      {
+        $this->userRoles[$rname] = Role::getRole($rid);
+      }
+
+  		/*while($row = $st->fetch( PDO::FETCH_ASSOC ) )
+  		{
+  			$this->userRoles[$row["role_name"]] = Role::getRole($row["role_id"]);
+  		}*/
+  	}
+
+  	public function hasPermission($permission)
+  	{
+  		foreach( $this->userRoles as $role )
+  		{
+  			if( $role->hasPermission($permission) )
+  			{
+  				return true;
+  			}
+  		}
+  		return false;
+  	}
+  }
