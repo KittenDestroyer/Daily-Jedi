e2ca8989b11ad4e84a9bc7bd445245e4d4179132
KittenDestroyer <o.androshchuk@internetdevels.ua>
2015-03-20 17:10:24 +0200

    Fucking cool

---
+++ test.php
@@ -0,0 +1,21 @@
+<html>
+ <head>
+   <style>
+     .pip {
+        float: left;
+        margin: 5px;
+        padding: 15px;
+        width: 300px;
+        height: 300px;
+        border: 1px solid black;
+        }
+   </style>
+ </head>
+ <body>
+  <ul>
+   <li class="pip">One</li>
+   <li class="pip">Two</li>
+   <li class="pip">Three</li>
+  </ul>
+ </body>
+</html>
